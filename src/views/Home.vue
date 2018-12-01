a13472124ef1abef08ae371070a6fa366dc9b6af
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-30 23:01:24 -0800

    Add Header Component in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <Advertisement />
+    <Header />
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <Footer />
@@ -11,7 +12,9 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 import Advertisement from '@/components/common/Advertisement.vue'
+import Header from '@/components/common/Header.vue'
 import Footer from '@/components/common/Footer.vue'
+
 
 import '../assets/css/fonts/IcoMoon-Free.ttf'
 import '../assets/css/icon/IcoMoon.css'
@@ -22,11 +25,34 @@ export default {
   components: {
     HelloWorld,
     Advertisement,
+    Header,
     Footer
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Nanum+Gothic');
 
+html, body, ul {
+  margin: 0;
+  padding: 0;
+  font-family: 'Nanum Gothic', sans-serif;
+}
+
+*, *::after, *::before { box-sizing:border-box; }
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+.clearfix {
+  overflow: hidden;
+  &:after {
+    display: block;
+    content:"";
+    clear: both;
+  }
+}
 </style>
