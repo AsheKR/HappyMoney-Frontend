f678ab0e9d64bad8d9c95ad23b356008a70827d8
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-12-02 04:55:15 -0800

    Etc Component 추가

---
+++ src/components/index/Etc.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="etc">
+    <div class="etcWrap" v-if="show">
+      <div class="firstEtc">
+        <div class="etc">
+          <a href="#"><img :src="require('@/assets/css/images/index/etc_1.gif')" alt=""></a>
+        </div>
+        <div class="etc">
+          <a href="#"><img :src="require('@/assets/css/images/index/etc_2.gif')" alt=""></a>
+        </div>
+      </div>
+      <div class="etc">
+        <a href="#"><img :src="require('@/assets/css/images/index/201809031609538.jpg')" alt=""></a>
+      </div>
+      <div class="etc">
+        <a href="#"><img src="http://image.happymoney.co.kr/extimage/banner/201808031508942.jpg" alt=""></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['events'],
+  data() {
+    return {
+      show: false
+    }
+  },
+  watch: {
+    events: function(newVal) {
+      this.childEvents = newVal;
+      this.show = !this.show;
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  #etc {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 10px;
+
+    .etcWrap {
+      display:grid;
+      grid-template-columns: 1fr 1fr 1fr;
+
+      > .firstEtc {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        border: 1px solid #ddd;
+
+        > .etc {
+          padding: 7px 0;
+
+          &:first-child {
+            border-bottom: 1px solid #ddd;
+          }
+        }
+      }
+
+      > .etc {
+
+        img {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+          vertical-align: top;
+        }
+      }
+    }
+  }
+
+  @media only screen and (max-width: 800px) {
+    .etcWrap {
+      display: flex !important;
+      flex-direction: column;
+    }
+  }
+</style>
