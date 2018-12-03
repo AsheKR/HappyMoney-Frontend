d1d4f8cadaab4df5a412cf8191e7435bd7bafa1a
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-12-02 20:57:35 -0800

    SignIn Component ,Router, View 구현

---
+++ src/views/DetailSignIn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="loginView">
+    <DetailSingIn />
+  </div>
+</template>
+
+<script>
+import DetailSingIn from '@/components/SignIn/DetailSingIn.vue'
+
+export default {
+  name: 'loginView',
+  data() {
+    return {
+      test: 0
+    }
+  },
+  components: {
+    DetailSingIn,
+  }
+}
+</script>
+
+<style lang="css">
+</style>
