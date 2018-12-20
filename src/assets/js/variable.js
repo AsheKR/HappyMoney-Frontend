b741d25c1be32f7b308fca30dc9f121e84022dd9
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-12-20 00:42:05 -0800

    배포 코드 수정

--- src/assets/js/variable.js
+++ src/assets/js/variable.js
@@ -1,5 +1,5 @@
-const hostname = "http://127.0.0.1:8000"
-// const hostname = "https://apis.ashe.kr"
+// const hostname = "http://127.0.0.1:8000"
+const hostname = "https://apis.ashe.kr"
 
 module.exports = {
   hostname: hostname,
